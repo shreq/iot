3ead72e7752598fae58dcb45d5e478a4fdad97e7
shreq <shrek.kresh@o2.pl>
2019-04-12 16:11:33 +0200

    finished

--- kenshiro/kenshiro.ino
+++ kenshiro/kenshiro.ino
@@ -7,10 +7,24 @@ and attempts to face towards that object. */
 #include <Wire.h>
 #include <Zumo32U4.h>
 
+Zumo32U4Buzzer buzzer;
 Zumo32U4LCD lcd;
 Zumo32U4Motors motors;
 Zumo32U4ProximitySensors proxSensors;
 Zumo32U4ButtonA buttonA;
+
+uint8_t musicPos = 0;
+const char music[] PROGMEM =
+  /*"! T120O5L16agafaea dac+adaea fa<aa<bac#a dac#adaea f"
+  "O6dcd<b-d<ad<g d<f+d<gd<ad<b- d<dd<ed<f+d<g d<f+d<gd<ad"
+  "L8MS<b-d<b-d MLe-<ge-<g MSc<ac<a MLd<fd<f O5MSb-gb-g"
+  "ML>c#e>c#e MS afaf ML gc#gc# MS fdfd ML e<b-e<b-"
+  "O6L16ragafaea dac#adaea fa<aa<bac#a dac#adaea faeadaca"
+  "<b-acadg<b-g egdgcg<b-g <ag<b-gcf<af dfcf<b-f<af"
+  "<gf<af<b-e<ge c#e<b-e<ae<ge <fe<ge<ad<fd"
+  "O5e>ee>ef>df>d b->c#b->c#a>df>d e>ee>ef>df>d"
+  "e>d>c#>db>d>c#b >c#agaegfe fO6dc#dfdc#<b c#4";*/
+  "! O3V24T90L4ML e2r11d11e11f11e11d11 ccrr11d11e11 ffr8f10f10f10e10 ddrd12c10d12 eer6e11f6d11 ccrc aar11a11b11>c11<b11a11 ggrr11f11e11 ffr11f11g11a11g11f11 eerc6c11 aar11a11b11>c11<b11a11 ggrr12f10e12 ffr11d11e11f11e11d11 c2r2";
 
 // A sensors reading must be greater than or equal to this
 // threshold in order for the program to consider that sensor as
@@ -56,7 +70,7 @@ uint16_t lastTimeObjectSeen = 0;
 
 void setup()
 {
-  proxSensors.initFrontSensor();
+  proxSensors.initThreeSensors();
 
   // Wait for the user to press A before driving the motors.
   lcd.clear();
@@ -94,12 +108,19 @@ void stop()
 
 void loop()
 {
+  if (!buzzer.isPlaying())
+  {
+    buzzer.playFromProgramSpace(music);
+  }
+  
   // Read the front proximity sensor and gets its left value (the
   // amount of reflectance detected while using the left LEDs)
   // and right value.
   proxSensors.read();
   uint8_t leftValue = proxSensors.countsFrontWithLeftLeds();
   uint8_t rightValue = proxSensors.countsFrontWithRightLeds();
+  uint8_t leftMidValue = proxSensors.countsLeftWithLeftLeds();
+  uint8_t rightMidValue = proxSensors.countsRightWithRightLeds();
 
   // Determine if an object is visible or not.
   bool objectSeen = leftValue >= sensorThreshold || rightValue >= sensorThreshold;
@@ -131,9 +152,7 @@ void loop()
 
     bool lastTurnRight = turnRight;
 
-   
-
-    if (leftValue < rightValue)
+    if (leftValue < rightValue || (leftValue == rightValue && rightMidValue > leftMidValue && rightMidValue > 3))
     {
       // The right value is greater, so the object is probably
       // closer to the robot's right LEDs, which means the robot
@@ -142,7 +161,7 @@ void loop()
       turnRight();
       senseDir = RIGHT;
     }
-    else if (leftValue > rightValue)
+    else if (leftValue > rightValue || (leftValue == rightValue && leftMidValue > rightMidValue && leftMidValue > 3))
     {
       // The left value is greater, so turn to the left.
       turnLeft();
@@ -151,8 +170,14 @@ void loop()
     else
     {
       // The values are equal, so stop the motors.
-      //stop();
-      goFront();
+      if (leftValue > 5 && rightValue > 5)
+      {
+        stop();
+      }
+      else
+      {
+        goFront();
+      }
     }
 
   }
@@ -170,13 +195,16 @@ void loop()
     {
       turnLeft();
     }
-    stop();
   }
 
   lcd.gotoXY(0, 0);
   lcd.print(leftValue);
   lcd.print(' ');
   lcd.print(rightValue);
+  lcd.print(' ');
+  lcd.print(leftMidValue);
+  lcd.print(' ');
+  lcd.print(rightMidValue);
   lcd.gotoXY(0, 1);
   lcd.print(turningRight ? 'R' : (turningLeft ? 'L' : ' '));
   lcd.print(' ');
